73e6a6bb7c95e509f30db28ff16d47576c7ae0f6
rakibhasansohag <rakibhasansohag133@gmail.com>
2026-01-26 23:19:17 +0600

    add morgan middleware for loggin errors

--- medistore-backend/src/app.ts
+++ medistore-backend/src/app.ts
@@ -1,5 +1,6 @@
 import { toNodeHandler } from 'better-auth/node';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import express, { Application } from 'express';
 
@@ -10,22 +11,27 @@ import { auth } from './lib/auth';
 
 const app: Application = express();
 
+// middlewares
+app.use(morgan('dev'));
 app.use(
 	cors({
 		origin: process.env.APP_URL || 'http://localhost:3000',
 		credentials: true,
 	}),
 );
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// routes (Better auth routes)
 app.all('/api/v1/auth/*splat', toNodeHandler(auth));
 
+// Check Health
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
 app.use(errorHandler);
+
 app.use(notFound);
 
 export default app;
