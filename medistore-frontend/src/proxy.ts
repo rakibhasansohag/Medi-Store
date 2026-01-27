6ae5d17dd1cf8a5ee6a180b4c70f6703afd7de67
rakibhasansohag <rakibhasansohag133@gmail.com>
2026-01-27 22:02:22 +0600

    proxy added for rediritng things

---
+++ medistore-frontend/src/proxy.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { userService } from './services/user.service';
+import { Roles } from './constants/roles';
+
+export async function proxy(request: NextRequest) {
+	const pathname = request.nextUrl.pathname;
+
+	let isAuthenticated = false;
+	let userRole: string | null = null;
+
+	const { data } = await userService.getSession();
+
+	if (data?.user) {
+		isAuthenticated = true;
+		userRole = data.user.role;
+	}
+
+	// Not authenticated - redirect to login
+	if (!isAuthenticated) {
+		return NextResponse.redirect(new URL('/login', request.url));
+	}
+
+	// Admin trying to access non-admin routes
+	if (userRole === Roles.admin && pathname.startsWith('/dashboard')) {
+		return NextResponse.redirect(new URL('/admin-dashboard', request.url));
+	}
+
+	// Non-admin trying to access admin routes
+	if (userRole !== Roles.admin && pathname.startsWith('/admin-dashboard')) {
+		return NextResponse.redirect(new URL('/dashboard', request.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ['/dashboard/:path*', '/admin-dashboard/:path*'],
+};
