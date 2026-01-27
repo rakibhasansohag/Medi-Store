60a469227443e04717fe835db42d3dd7a5f07b35
rakibhasansohag <rakibhasansohag133@gmail.com>
2026-01-27 21:25:29 +0600

    frontend category service added for fetching data

---
+++ medistore-frontend/src/services/category.service.ts
@@ -0,0 +1,39 @@
+import { env } from '@/env';
+import { IApiResponse, ICategory } from '@/types';
+
+const API_URL = env.API_URL;
+
+export const categoryService = {
+	getCategories: async (): Promise<IApiResponse<{ data: ICategory[] }>> => {
+		try {
+			const res = await fetch(`${API_URL}/categories`, {
+				next: { tags: ['categories'] },
+			});
+
+			const data = await res.json();
+			return { success: true, message: 'Success', data };
+		} catch (error) {
+			return {
+				success: false,
+				message: 'Failed to fetch categories',
+				details: error,
+			};
+		}
+	},
+
+	getCategoryById: async (
+		id: string,
+	): Promise<IApiResponse<{ data: ICategory }>> => {
+		try {
+			const res = await fetch(`${API_URL}/categories/${id}`);
+			const data = await res.json();
+			return { success: true, message: 'Success', data };
+		} catch (error) {
+			return {
+				success: false,
+				message: 'Failed to fetch category',
+				details: error,
+			};
+		}
+	},
+};
