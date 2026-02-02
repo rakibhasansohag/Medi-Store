7f8bb5f50182502d70f18ab9659a5a32c7b66b0d
Rakib Hasan Sohag <rakibhasansohag133@gmail.com>
2026-02-02 23:54:29 +0600

    v1

--- medistore-frontend/src/lib/auth-client.ts
+++ medistore-frontend/src/lib/auth-client.ts
@@ -4,7 +4,7 @@ export const authClient = createAuthClient({
 	/** The base URL of the server (optional if you're using the same domain) */
 	baseURL:
 		typeof window !== 'undefined'
-			? `${window.location.origin}/v1`
+			? `${window.location.origin}/api/v1`
 			: process.env.NEXT_PUBLIC_AUTH_URL ?? '',
 	fetchOptions: {
 		credentials: 'include',
